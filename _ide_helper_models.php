5c8a5a758effaeb6b5b1bd98c76a575c9278c5bb
filip <filipmitrovic90@gmail.com>
2020-06-17 13:53:42 +0200

    initial commit

---
+++ _ide_helper_models.php
@@ -0,0 +1,98 @@
+<?php
+
+// @formatter:off
+/**
+ * A helper file for your Eloquent Models
+ * Copy the phpDocs from this file to the correct Model,
+ * And remove them from this file, to prevent double declarations.
+ *
+ * @author Barry vd. Heuvel <barryvdh@gmail.com>
+ */
+
+
+namespace App{
+/**
+ * App\User
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $email
+ * @property \Illuminate\Support\Carbon|null $email_verified_at
+ * @property string $password
+ * @property string|null $remember_token
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Illuminate\Notifications\DatabaseNotificationCollection|\Illuminate\Notifications\DatabaseNotification[] $notifications
+ * @property-read int|null $notifications_count
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User query()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereEmail($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereEmailVerifiedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User wherePassword($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereRememberToken($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereUpdatedAt($value)
+ */
+	class User extends \Eloquent {}
+}
+
+namespace App{
+/**
+ * App\Contact
+ *
+ * @property int $id
+ * @property string $first_name
+ * @property string $last_name
+ * @property string|null $phone
+ * @property string $email
+ * @property string $address
+ * @property int $company_id
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \App\Company $company
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Contact newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Contact newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Contact query()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Contact whereAddress($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Contact whereCompanyId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Contact whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Contact whereEmail($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Contact whereFirstName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Contact whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Contact whereLastName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Contact wherePhone($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Contact whereUpdatedAt($value)
+ */
+	class Contact extends \Eloquent {}
+}
+
+namespace App{
+/**
+ * App\Company
+ *
+ * @property int $id
+ * @property string $name
+ * @property string|null $address
+ * @property string|null $website
+ * @property string $email
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Contact[] $contacts
+ * @property-read int|null $contacts_count
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Company newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Company newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Company query()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Company whereAddress($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Company whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Company whereEmail($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Company whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Company whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Company whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Company whereWebsite($value)
+ */
+	class Company extends \Eloquent {}
+}
+
